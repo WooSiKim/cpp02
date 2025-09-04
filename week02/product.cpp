a8222486e9e55c8bf45f258f79c5c4a15a276d52
unknown <rlatldn01@email.daelim.ac.kr>
2025-09-04 11:18:09 +0900

    .gitignore & .h & .cpp

---
+++ week02/product.cpp
@@ -0,0 +1,13 @@
+#include "product.h"
+
+Product::Product(string name, double unitPrice)
+	: name(name), unitPrice(unitPrice)
+{
+}
+Product::~Product()
+{
+}
+double Product::getPrice() const
+{
+	return unitPrice;
+}
