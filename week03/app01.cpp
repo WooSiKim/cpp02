66acbbb3a3070e0274a51747416203c75619bbee
unknown <rlatldn01@email.daelim.ac.kr>
2025-09-11 12:17:01 +0900

    week03 v0.3 메모리누수방지

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -7,7 +7,7 @@ class Pokemon
 {
 public:
 	Pokemon() { cout << "포켓몬 생성자\n"; }
-	~Pokemon() { cout << "포켓몬 소멸자\n"; }
+	virtual~Pokemon() { cout << "포켓몬 소멸자\n"; }
 	virtual void attack() const { cout << "공격 !" << endl; }
 };
 
