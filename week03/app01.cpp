8470accc5705c683072228e1d6bb564a6b3c4338
unknown <rlatldn01@email.daelim.ac.kr>
2025-09-11 12:08:25 +0900

    week03 v0.3 destructor

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -6,13 +6,17 @@ using namespace std;
 class Pokemon
 {
 public:
+	Pokemon() { cout << "포켓몬 생성자\n"; }
+	~Pokemon() { cout << "포켓몬 소멸자\n"; }
 	virtual void attack() const { cout << "공격 !" << endl; }
 };
 
 class Pikachu : public Pokemon
 {
 public:
-	virtual void attack() const { cout << "전기 공격 !" << endl; }
+	Pikachu() { cout << "피카츄 생성자\n"; }
+	~Pikachu() { cout << "피카츄 소멸자\n"; }
+	void attack() const { cout << "전기 공격 !" << endl; }
 };
 int main()
 {
