2c84a7ce3bfb0d8933c1a285d7091ca25a382f6e
unknown <rlatldn01@email.daelim.ac.kr>
2025-09-18 12:26:11 +0900

    week04 v0.4 pointer array

---
+++ week04/squirtle.cpp
@@ -0,0 +1,16 @@
+#include "squirtle.h"
+
+Squirtle::Squirtle()
+{
+	cout << "部何扁 积己磊\n";
+}
+
+Squirtle::~Squirtle()
+{
+	cout << "部何扁 家戈磊\n";
+}
+
+void Squirtle::attack() const
+{
+	cout << "拱措器 傍拜 !" << endl;
+}
---
+++ week04/squirtle.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "pokemon.h"
+class Squirtle : public Pokemon
+{
+public:
+	Squirtle();
+	~Squirtle();
+	void attack() const;
+};
