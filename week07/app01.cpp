657f8a75683063402d16d01e4b0b09f9aafab621
unknown <rlatldn01@email.daelim.ac.kr>
2025-09-25 18:21:45 +0900

    week07 v1.4 virtual inheritance

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -4,16 +4,16 @@ using namespace std;
 class Person {
 public:
 	string name;
-	virtual void warn() = 0;
+	//virtual void warn() = 0;
 };
-class UndergraduateStudent : public Person{
+class UndergraduateStudent : virtual public Person{
 public:
 	double gpa;
 	void warn() { cout << "학사경고\n"; };
 
 };
 
-class DormitoryStudent : public Person{
+class DormitoryStudent : virtual public Person{
 public:
 	int rommNumber;
 	void warn() { cout << "벌점부여\n"; };
@@ -28,7 +28,10 @@ int main() {
 	UndergraduateDormitoryStudent uds;
 	//uds.warn();
 	uds.DormitoryStudent::warn();
-	//uds.name = "Kim" /죽음의다이아몬드
+	uds.gpa = 3.9;
+	uds.rommNumber = 201;
+	uds.name = "KIM";
 
+	cout << uds.name << "의 평점은 "<<uds.gpa << "이고, 기숙사 방 번호는 "<<uds.rommNumber <<"입니다.\n";
 	return 0;
 }
