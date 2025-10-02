40c6d7f18de817781c39059f5cf3b5215dcd8304
unknown <rlatldn01@email.daelim.ac.kr>
2025-10-02 11:31:29 +0900

    week06 v1.7 operator overloading

--- week06/app01.cpp
+++ week06/app01.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 class Complex {
@@ -6,12 +6,12 @@ private:
 	int real;
 	int imaginary;
 public:
-	Complex() : real(0), imaginary(0){
-
+	Complex() : real(0), imaginary(0) {
 	}
 	Complex(int real, int imaginary) : real(real), imaginary(imaginary) {
-		
+
 	}
+	//void setReal(int real) : real(real){
 	void setReal(int real) {
 		//real = real;
 		this->real = real;
@@ -22,13 +22,23 @@ public:
 	void setImaginary(int imaginary) {
 		this->imaginary = imaginary;
 	}
-	int getImaginary() const {
+	int getimaginary() const {
 		return imaginary;
 	}
 	Complex operator+(const Complex& right) {
 		int r = this->real + right.real;
 		int i = this->imaginary + right.imaginary;
-		return Complex(r,i);
+		return Complex(r, i);
+		//return Complex(r, this->imaginary);  // 가능하나 수학적으로 문제가 됨
+	}
+	Complex operator++(int) {  // 후위연산
+		Complex previous(this->real, this->imaginary);
+		this->real = this->real + 1;
+		return previous;
+	}
+	Complex operator++() {  // 전위연산
+		this->real++;
+		return Complex(this->real, this->imaginary);
 	}
 };
 
@@ -37,11 +47,20 @@ int main() {
 	Complex c2(10, 7);
 	c1.setReal(5);
 	c1.setImaginary(3);
-	cout << c1.getReal() << " + " << c1.getImaginary() << "i" << endl;
-	cout << c2.getReal() << " + " << c2.getImaginary() << "i" << endl;
+	//cout << c1.getReal() << "+" << c1.getimaginary() << "i" << endl;
+	//cout << c2.getReal() << "+" << c2.getimaginary() << "i" << endl;
 
-	Complex c3 = c1 + c2;
-	cout << c3.getReal() << " + " << c3.getImaginary() << "i" << endl;
+	Complex c3 = c1 + c2;  // Complex c3 = c1.operator+(c2);
+	//cout << c3.getReal() << "+" << c3.getimaginary() << "i" << endl;
+
+	//Complex c4 = c3++;	
+	Complex c4 = ++c3;
+	cout << c3.getReal() << "+" << c3.getimaginary() << "i" << endl;
+	cout << c4.getReal() << "+" << c4.getimaginary() << "i" << endl;
+
+	//int i1 = 15;
+	//int i2 = ++i1;
+	//cout << i2 << endl;
 
 	return 0;
 }
